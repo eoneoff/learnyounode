0eb1fdacc901457edf8fffaa0228dd44ef907019
eoneoff <eoneoff@gmail.com>
2019-09-21 10:12:18 +0300

    10 lessons completed

--- time-server.js
+++ time-server.js
@@ -1,2 +1,6 @@
-"use strict";git 
+"use strict";
 const net = require('net');
+net.createServer((socket) =>{
+    let date = new Date();
+    socket.end(`${date.getFullYear()}-${(date.getMonth()< 9 ? 0 : '')}${date.getMonth()+1}-${date.getDate() < 10 ? '0':''}${date.getDate()} ${date.getHours() < 10 ? '0':''}${date.getHours()}:${date.getMinutes() < 10 ? '0':''}${date.getMinutes()}\n`);
+}).listen(process.argv[2] || 8000);
